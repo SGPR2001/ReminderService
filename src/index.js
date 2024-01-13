8dc16342169bcdd526a63352e91bccb9aff4683a
SGPR2001 <sgddsp@gmail.com>
2024-01-13 12:53:29 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-// const apiRoutes = require("./routes/index");
-// const db = require("./models/index");
-
+const {sendBasicEmail}=require('./services/email-service')
 const app = express();
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-//   app.use("/api", apiRoutes);
+
   app.listen(PORT, async () => {
     console.log(`Server Started on Port:${PORT}`);
-    if (process.env.DB_SYNC) {
-      db.sequelize.sync({ alter: true });
-    }
+    sendBasicEmail(
+        'support@admin.com',
+        'sgddsp@gmail.com',
+        'This is a testing email',
+        'Hey, how are you,I hope you like the support'
+    )
+   
   });
 };
 prepareAndStartServer();
