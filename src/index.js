cea629625a6d55ef0ec7e5d13d96b6c32a053e94
SGPR2001 <sgddsp@gmail.com>
2024-01-15 07:57:46 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-const {sendBasicEmail}=require('./services/email-service')
+// const {sendBasicEmail}=require('./services/email-service')
 const app = express();
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
@@ -9,12 +9,12 @@ const prepareAndStartServer = () => {
 
   app.listen(PORT, async () => {
     console.log(`Server Started on Port:${PORT}`);
-    sendBasicEmail(
-        'support@admin.com',
-        'sgddsp@gmail.com',
-        'This is a testing email',
-        'Hey, how are you,I hope you like the support'
-    )
+    // sendBasicEmail(
+    //     'support@admin.com',
+    //     'sgddsp@gmail.com',
+    //     'This is a testing email',
+    //     'Hey, how are you,I hope you like the support'
+    // )
    
   });
 };
