377cb398e5e776cd7b989a103e68b4283cd8ea8f
SGPR2001 <sgddsp@gmail.com>
2024-01-17 11:25:01 +0530

    updated messagequeue

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-// const {sendBasicEmail}=require('./services/email-service')
+const {createChannel,subscribeMessage}=require("./utils/messageQueue")
+const{REMINDER_BINDING_KEY}=require("./config/serverConfig")
+
 const jobs=require('./utils/job')
 const TicketController=require('./controllers/ticket-controller')
+const EmailService =require("./services/email-service")
 const app = express();
-const prepareAndStartServer = () => {
+const prepareAndStartServer = async() => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  const channel=await createChannel()
+  subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY)
   app.post('/api/v1/tickets',TicketController.create)
   app.listen(PORT, async () => {
     console.log(`Server Started on Port:${PORT}`);
-    jobs();
-    // sendBasicEmail(
-    //     'support@admin.com',
-    //     'sgddsp@gmail.com',
-    //     'This is a testing email',
-    //     'Hey, how are you,I hope you like the support'
-    // )
+    // jobs();
+    
    
   });
 };
